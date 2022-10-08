7bdef91c4213b3f8ba3810fb78f96880bb9d98a7
Alexander Titov <titov.alex627@gmail.com>
2022-10-09 00:32:57 +0300

    feature/swagger: add swagger (#1)
    Co-authored-by: Alexander Titov1 <alexander_titov1@epam.com>

--- src/features/comments/schemas/comment.schema.ts
+++ src/features/comments/schemas/comment.schema.ts
@@ -1,37 +1,51 @@
-import { Prop, Schema, SchemaFactory, raw } from '@nestjs/mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import * as mongoose from 'mongoose'
+import { ApiProperty } from '@nestjs/swagger'
 
 import { User } from '../../users'
 
+import { ReplyComment } from '../entities/reply-comment.entity'
+
 export type CommentDocument = Comment & mongoose.Document
 
-@Schema()
+@Schema({ timestamps: true })
 export class Comment {
+  @ApiProperty({ type: String })
   @Prop({ type: String, required: true })
   content: string
 
-  @Prop({ type: Number, required: true })
+  @ApiProperty({ type: Number, minimum: 0, default: 0 })
+  @Prop({ type: Number, required: true, default: 0, min: 0 })
   score: number
 
+  @ApiProperty({ type: User })
   @Prop({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   })
-  user: User
+  user: mongoose.Schema.Types.ObjectId
 
-  @Prop(raw({
-    id: { type: String, required: true },
-    content: { type: String, required: true },
-    score: { type: Number, required: true },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    replyingTo: { type: String, required: true },
-  }))
-  replies: Array<Record<string, any>>
+  @ApiProperty({
+    type: ReplyComment,
+    isArray: true,
+    // default: [],
+  })
+  @Prop({
+    type: [{
+      id: { type: String, required: true },
+      content: { type: String, required: true },
+      score: { type: Number, required: true },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      replyingTo: { type: String, required: true },
+    }],
+    default: [],
+  })
+  replies: ReplyComment[]
 }
 
 export const CommentSchema = SchemaFactory.createForClass(Comment)
