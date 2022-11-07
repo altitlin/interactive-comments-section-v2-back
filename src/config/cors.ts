b31901eada042fff24eeaf946f24b6fe58b25457
Alexander Titov <titov.alex627@gmail.com>
2022-11-07 16:06:26 +0300

    fix/cors-url: fix error CORS (#15)
    Co-authored-by: Alexander Titov1 <alexander_titov1@epam.com>

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 export const corsOptions: CorsOptions = {
-  origin: process.env.FRONT_URL,
+  origin: process.env.FRONT_URL ?? 'https://localhost:4242',
   methods: [ 'GET', 'POST', 'PUT', 'DELETE' ],
-  allowedHeaders: [ 'content-type' ],
+  allowedHeaders: [ 'Content-Type' ],
 }
